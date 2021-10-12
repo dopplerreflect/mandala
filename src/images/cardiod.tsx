685cfc4a3a63d63caf173df916bbc440d8001ebe
David Rose <doppler@gmail.com>
2021-10-12 10:04:23 -0400

    tweek cardiod

--- src/images/cardiod.tsx
+++ src/images/cardiod.tsx
@@ -5,7 +5,9 @@ const Cardiod = () => {
   const width = 1080;
   const c = width / 2;
   const r = (c / 10) * 9;
-  const d = 2.25;
+  const ad = 2.25;
+  const gd = 5;
+  const a = [...Array(gd).keys()].map(k => (360 / gd) * k);
   const rp = (angle: number, radius: number): Point =>
     radialPoint(angle, radius, { x: c, y: c });
 
@@ -14,24 +16,26 @@ const Cardiod = () => {
   return (
     <SVGTag width={width} height={width}>
       <rect width={width} height={width} fill="black" />{' '}
-      <g id="cardioid" transform={`rotate(90, ${c}, ${c})`}>
-        {[...Array(360 / d).keys()].map(a => (
-          <circle
-            key={a}
-            cx={
-              rp(a * d, (r / 2) * (1 - Math.cos(polar(a * d)))).x * PHI ** 0.5
-            }
-            cy={rp(a * d, (r / 2) * (1 - Math.cos(polar(a * d)))).y}
-            r={r * PHIm1 ** 10}
-            // stroke="white"
-            fill={`hsl(${a * d - 30}, 100%, 50%)`}
-          />
-        ))}
-      </g>
-      <use xlinkHref="#cardioid" transform={`rotate(72, ${c}, ${c})`} />
-      <use xlinkHref="#cardioid" transform={`rotate(144, ${c}, ${c})`} />
-      <use xlinkHref="#cardioid" transform={`rotate(216, ${c}, ${c})`} />
-      <use xlinkHref="#cardioid" transform={`rotate(288, ${c}, ${c})`} />
+      <defs>
+        <g id="cardioid" transform={`rotate(90, ${c}, ${c})`}>
+          {[...Array(360 / ad).keys()].map(a => (
+            <circle
+              key={a}
+              cx={
+                rp(a * ad, (r / 2) * (1 - Math.cos(polar(a * ad)))).x *
+                PHI ** PHIm1
+              }
+              cy={rp(a * ad, (r / 2) * (1 - Math.cos(polar(a * ad)))).y}
+              r={r * PHIm1 ** 10}
+              // stroke="white"
+              fill={`hsl(${a * ad - 30}, 100%, 50%)`}
+            />
+          ))}
+        </g>
+      </defs>
+      {a.map(a => (
+        <use xlinkHref="#cardioid" transform={`rotate(${a}, ${c}, ${c})`} />
+      ))}
     </SVGTag>
   );
 };
