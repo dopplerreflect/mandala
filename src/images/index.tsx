3a74d1e5e2ea6763081b87e0be5fb737edd1edd0
David Rose <doppler@gmail.com>
2021-10-12 16:22:25 -0400

    comment out older, less inspiring works

--- src/images/index.tsx
+++ src/images/index.tsx
@@ -1,8 +1,8 @@
-export { FourArc } from './four-arc';
-export { FiveArc } from './five-arc';
+// export { FourArc } from './four-arc';
+// export { FiveArc } from './five-arc';
 export { SixArc } from './six-arc';
-export { StarTangles } from './star-tangles';
-export { SplitStar } from './split-star';
+// export { StarTangles } from './star-tangles';
+// export { SplitStar } from './split-star';
 export { TimesTable } from './times-table';
 export { StarMandala } from './star-mandala';
 export { CornerReflector } from './corner-reflector';
