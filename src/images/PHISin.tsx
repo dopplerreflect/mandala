9232e2fb8e4faa6278e6286e7486c81b32900d38
David Rose <doppler@gmail.com>
2021-11-08 20:23:03 -0500

    prettiyf PHISin

--- src/images/PHISin.tsx
+++ src/images/PHISin.tsx
@@ -9,25 +9,52 @@ export default () => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox={`${-width / 2} ${-width / 2} ${width} ${width}`}
     >
+      <defs>
+        <radialGradient id="bgGradient">
+          <stop offset="0%" stopColor="hsl(240, 100%, 50%" />
+          <stop offset="100%" stopColor="hsl(270, 100%, 50%" />
+        </radialGradient>
+      </defs>
       <path
-        d={`M${-width / 2},${-width / 2}H${width}V${width}H${-width / 2}Z`}
-        fill="black"
+        d={`M${-width / 2},${-width / 2}H${width / 2}V${width / 2}H${
+          -width / 2
+        }Z`}
+        fill="url(#bgGradient)"
       />
-      {[...Array(8).keys()].map(i => {
-        return [...Array(width / 24).keys()].map(k => {
-          const x = -width / 2 + k * 24;
+      {[...Array(12).keys()].map(i => (
+        <g key={i} stroke="white" strokeWidth="2">
+          <path
+            d={`M${-width / 2},${(-width / 2) * (PHI - 1) ** i}H${width / 2}`}
+          />
+          <path
+            d={`M${-width / 2},${(width / 2) * (PHI - 1) ** i}H${width / 2}`}
+          />
+          <circle r={(width / 2) * (PHI - 1) ** i} fill="none" />
+        </g>
+      ))}
+      {[...Array(10).keys()].map(i => {
+        const points = [...Array(width / 15).keys()].map(k => {
+          const x = -width / 2 + k * 15;
           const y = Math.sin((PI2 / width) * x) * (width / 2) * (PHI - 1) ** i;
-          return (
-            <circle
-              key={`i${i}k${k}`}
-              cx={x}
-              cy={y}
-              r={(width / 1080) * 4}
-              fill="none"
-              stroke={`hsl(${(360 / width / 2) * x}, 100%, 50%)`}
-            />
-          );
+          return { x, y };
         });
+        const [first, ...rest] = points;
+        const d = [
+          `M${first.x},${first.y}`,
+          rest.map(point => [`L${point.x},${point.y}`].join(' ')),
+          `L${width / 2},0`,
+          // 'Z',
+        ].join(' ');
+        return (
+          <path
+            key={i}
+            d={d}
+            strokeWidth={2}
+            stroke={`hsl(${6 * i}, 100%, 50%)`}
+            fill={`hsl(${6 * i}, 100%, 5%)`}
+            fillOpacity="0.5"
+          />
+        );
       })}
     </svg>
   );
