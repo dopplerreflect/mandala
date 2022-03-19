4bd5df7c593efd7d26c2fbc36ba1910f9e5c9cd1
David Rose <doppler@gmail.com>
2022-03-18 19:31:58 -0500

    NestedPentagrams: sharpen up

--- src/images/NestedPentagrams.tsx
+++ src/images/NestedPentagrams.tsx
@@ -26,7 +26,7 @@ export default function NestedPentagrams() {
 
   const stroke = 'hsl(225, 100%, 50%)';
   const fill = 'hsl(45, 100%, 50%)';
-  const fillOpacity = 0.75;
+  const fillOpacity = 1;
 
   return (
     <svg
@@ -35,23 +35,30 @@ export default function NestedPentagrams() {
       xmlns='http://www.w3.org/2000/svg'
       viewBox={`${-width / 2} ${-width / 2} ${width} ${width}`}
     >
+      <defs></defs>
       <path
         d={`M${-width / 2},${-width / 2}H${width}V${width}H${-width / 2}Z`}
         fill='hsl(240, 50%, 95%)'
       />
-      <g id='blueprint'>
-        {[...Array(12).keys()].map(r => (
-          <g key={r}>
-            <circle
-              r={outerRadius - innerRadius * PHIm1 ** (r - 1)}
-              stroke={stroke}
-              fill={'none'}
-              fillOpacity={fillOpacity}
-            />
+      <path
+        id='centerStar'
+        d={starPath(innerRadius * PHIm1 ** 2)}
+        stroke={stroke}
+        fill={fill}
+        fillOpacity={fillOpacity}
+      />
+      <g id='centerSurroundingStars'>
+        {[...Array(5).keys()].map(k => (
+          <g key={k} id={`centerSurroundingStar-${k}`}>
             <path
-              d={starPath(outerRadius - innerRadius * PHIm1 ** (r - 1))}
+              d={starPath(
+                innerRadius * PHIm1 ** 2,
+                radialPoint(angles10[0], innerRadius * PHIm1)
+              )}
               stroke={stroke}
-              fill='none'
+              fill={fill}
+              fillOpacity={fillOpacity}
+              transform={`rotate(${72 * k + 36})`}
             />
           </g>
         ))}
@@ -61,6 +68,7 @@ export default function NestedPentagrams() {
           <g key={r} id={`ring-${r}`}>
             {[...Array(5).keys()].map(k => (
               <path
+                id={`star-ring-${r}-ray-${k}`}
                 key={k}
                 d={starPath(
                   innerRadius * (PHI - 1) ** (r + 1),
@@ -79,27 +87,24 @@ export default function NestedPentagrams() {
           </g>
         ))}
       </g>
-      <path
-        d={starPath(innerRadius * PHIm1 ** 2)}
-        stroke={stroke}
-        fill={fill}
-        fillOpacity={fillOpacity}
-      />
-      {[...Array(5).keys()].map(k => (
-        <g key={k}>
-          <path
-            d={starPath(
-              innerRadius * PHIm1 ** 2,
-              radialPoint(angles10[0], innerRadius * PHIm1)
-            )}
-            stroke={stroke}
-            fill={fill}
-            fillOpacity={fillOpacity}
-            transform={`rotate(${72 * k + 36})`}
-          />
-        </g>
-      ))}
-      {/* <use href='#starStar' transform='rotate(36)' /> */}
+
+      <g id='blueprint'>
+        {[...Array(12).keys()].map(r => (
+          <g key={r}>
+            <circle
+              r={outerRadius - innerRadius * PHIm1 ** (r - 1)}
+              stroke={stroke}
+              fill={'none'}
+              fillOpacity={fillOpacity}
+            />
+            <path
+              d={starPath(outerRadius - innerRadius * PHIm1 ** (r - 1))}
+              stroke={stroke}
+              fill='none'
+            />
+          </g>
+        ))}
+      </g>
     </svg>
   );
 }
